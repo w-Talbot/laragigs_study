be02de8dca34de61f8d4d9ed4a00a2c5490ed81d
w-talbot <wesstalbot@gmail.com>
2022-08-16 12:10:10 +0100

    first commit

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/hello', function() {
+   return response('<h1>HThis is being turned into plain text by the header, ignoring the html elements : <h1>', 200)
+   ->header('Content-Type','text/plain')
+   ->header('foo','bar');
+});
+
+Route::get('/posts/{id}', function($id){
+   return response('Your number is : ' . $id);
+
+}) ->where('id','[0-9]+');
+
